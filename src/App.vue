389e092e704c30d665992b0a472e0b76221338fe
Denis <bresgas.ru@mail.ru>
2021-08-25 18:02:13 +0500

    form
    created a form

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <vueComp />
+  </div>
+</template>
+
+<script>
+import vueComp from '@/components/vueComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    vueComp
+  }
+}
+</script>
+
+
